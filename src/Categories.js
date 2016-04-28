b903e972b3e76c9818d3d4652a1c95c152104fe8
jakeprins <jakeprins@outlook.com>
2016-04-28 16:40:27 +0200

    Categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import jQuery from 'jquery';
+import { Link } from 'react-router'
 
 class Categories extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      categories: []
+    };
+  }
+  componentDidMount () {
+    this.getCategories()
+  }
+
+  getCategories() {
+    let component = this;
+    let url = "https://nameless-dawn-52576.herokuapp.com/categories.json";
+    jQuery.getJSON(url, function(data) {
+      component.setState({
+        categories: data.categories
+      })
+    });
+  }
   render() {
     return (
+      <div className="categories">
         <h1>Categories!</h1>
+        <ul>
+        {this.state.categories.map(function(category){
+          return (
+            <li key={category.id}>
+            <link to={'/categories/${category.id}'}>{category.name}</link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
     );
   }
 }
