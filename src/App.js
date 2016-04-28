25b308b087db4eb14e660de272f851c62691505d
jakeprins <jakeprins@outlook.com>
2016-04-28 15:47:48 +0200

    Routes etc.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div className="container">
+              <h1>VideoGame Reviews, yeah!</h1>
+              
+              {this.props.children}
+            </div>
         );
     }
 }
